94b0a44575e906f9bd8b75d852411b39b618a5ac
Neil Sabde <70092545+hackorum@users.noreply.github.com>
2020-08-25 19:39:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var r,g,b,a,x,y;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function draw() {
+  a=random(20,100);
+  r=random(0,255);
+  g=random(0,255);
+  b=random(0,255);
+  x=random(0,windowWidth);
+  y=random(0,windowHeight);
+  fill(r,g,b,a);
+  circle(x,y,30);
+}
+
+function mousePressed(){
+ background(r,g,b);
+}
